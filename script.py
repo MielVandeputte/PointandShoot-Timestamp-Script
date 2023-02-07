9205a0466619967640c2b017bb95610ee8b96486
Miel Vandeputte <vandeputte.miel@gmail.com>
2023-02-07 12:34:55 +0100

    add timestamp

--- script.py
+++ script.py
@@ -1,17 +1,18 @@
 import os
+import math
 from PIL import Image, ImageFont, ImageDraw
 from datetime import datetime
-import math
+from tqdm import tqdm
 
-print('Timestamp adder script started...\n')
+scale = int(input('Logo scale (recommended value is 5): '))
+text_size = int(input('Text size (recommended value is 64): '))
 
+print('\nTimestamp adder script started...')
 logo = Image.open('logo.png')
 
-for file in os.listdir('.'):
+for file in tqdm(os.listdir('.')):
 
     if file.split('.')[-1].lower() in ['png', 'jpeg', 'jpg', 'ppm', 'gif', 'tiff', 'bmp']:
-
-        print(f'Opening {file}:\t', end='')
 
         try:
             img = Image.open(file)
@@ -26,15 +27,17 @@ for file in os.listdir('.'):
                 raise Exception('No datetime value is found in exif data')
 
             datetime_form = datetime.strptime(datetime_str, '%Y:%m:%d %H:%M:%S').strftime('%d.%m.%Y %H:%M')
-            print(datetime_form)
 
-            font = ImageFont.truetype('OpenSans-SemiBold.ttf')
+            font = ImageFont.truetype('OpenSans-SemiBold.ttf', text_size)
             img_editable = ImageDraw.Draw(img)
-            img_editable.text((5000,3000), datetime_form, (237, 230, 211), font=font)
+
+            text_width, text_height = img_editable.textbbox(datetime_form, font=font)
+
+            img_editable.text((math.floor(img.size[0]/2 - text_width/2), img.size[1] - 20 - text_height), datetime_form, fill=(255, 255, 255), stroke_fill=(0, 0, 0), stroke_width=2, font=font)
 
             # Add logo
-            logo = logo.resize((math.floor(img.size[0]/10), math.floor(img.size[0]/10*logo.size[1]/logo.size[0])))
-            img.paste(logo, (math.floor(img.size[0]/2 - logo.size[0]/2), img.size[1] - logo.size[1] - 20), logo)
+            logo = logo.resize((math.floor(img.size[0]/scale), math.floor(img.size[0]/scale*logo.size[1]/logo.size[0])))
+            img.paste(logo, (math.floor(img.size[0]/2 - logo.size[0]/2), img.size[1] - logo.size[1] - 20 - text_height), logo)
 
             # Save modified image
             if not os.path.exists('result'):
@@ -43,9 +46,4 @@ for file in os.listdir('.'):
             img.save(f'result/{file}')
         
         except Exception as e:
-            print(e)
-
-    
-            
-
-    
+            pass
